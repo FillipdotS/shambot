a3cd71bc4c94906e9ac22dba74937b38aefe4474
Fillip Serov <fillip.serov@gmail.com>
2022-10-09 18:00:43 +0100

    Add and use new PG_HOST env var

--- src/db/pool.js
+++ src/db/pool.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
 	user: process.env.SHAMBOT_PG_USER,
 	password: process.env.SHAMBOT_PG_PASS,
-	host: 'localhost',
+	host: process.env.SHAMBOT_PG_HOST,
 	database: 'shambot-db',
 	port: 5432,
 });
