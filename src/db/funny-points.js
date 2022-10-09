6775854cbe9a8f1ebdf2314fbdef59294c92d5fc
Fillip Serov <fillip.serov@gmail.com>
2022-10-09 18:00:53 +0100

    Fix null error

--- src/db/funny-points.js
+++ src/db/funny-points.js
@@ -3,7 +3,7 @@ const pool = require('./pool.js');
 const getPointsForUser = async ({ userId }) => {
 	const result = await pool.query('SELECT funny_points FROM users WHERE id = $1', [userId]);
 
-	return result.rows[0]['funny_points'] || 0;
+	return result.rows[0]?.['funny_points'] || 0;
 };
 
 module.exports = {
